0f90f3debd616b9a7495d452eea3b68ff3167092
cidkidnix <cidkidnix@protonmail.com>
2023-02-23 11:31:35 -0600

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs: let
+    lib = import "${inputs.nixpkgs}/lib";
+    supportedSystems = lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    pkgs' = supportedSystems (system: inputs.nixpkgs.legacyPackages.${system});
+  in {
+    packages = supportedSystems (system: let
+      pkgs = pkgs'.${system};
+    in {
+      rebuild = pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "rebuild" ./. { }) {
+        librarySystemDepends = [
+          pkgs.nix
+        ];
+      };
+    });
+  };
+}
